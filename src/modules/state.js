b771d3c0b9fab50bd20632f7b873e619641fcce3
George <71434803+george-swift@users.noreply.github.com>
2021-06-03 12:06:45 +0100

    Add prototype to change state depending on keys and actions

--- src/modules/state.js
+++ src/modules/state.js
@@ -1,4 +1,11 @@
-export default class State {
+function overlap(actor1, actor2) {
+  return actor1.pos.x + actor1.size.x > actor2.pos.x
+         && actor1.pos.x < actor2.pos.x + actor2.size.x
+         && actor1.pos.y + actor1.size.y > actor2.pos.y
+         && actor1.pos.y < actor2.pos.y + actor2.size.y;
+}
+
+class State {
   constructor(level, actors, status) {
     this.level = level;
     this.actors = actors;
@@ -9,7 +16,30 @@ export default class State {
     return new State(level, level.startActors, 'playing');
   }
 
-  get Player() {
+  get player() {
     return this.actors.find((a) => a.type === 'player');
   }
 }
+
+State.prototype.update = function gridBoundary(time, keys) {
+  const actors = this.actors.map((actor) => actor.update(time, this, keys));
+  let newState = new State(this.level, actors, this.status);
+
+  if (newState.status !== 'playing') return newState;
+
+  const { player } = newState;
+
+  if (this.level.touches(player.pos, player.size, 'lava')) {
+    return new State(this.level, actors, 'lost');
+  }
+
+  actors.forEach((actor) => {
+    if (actor !== player && overlap(actor, player)) {
+      newState = actor.collide(newState);
+    }
+  });
+
+  return newState;
+};
+
+export default State;
