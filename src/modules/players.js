1eae46b649ce0e3bee6a6bc4ead78cc42b5932d2
George <71434803+george-swift@users.noreply.github.com>
2021-06-03 12:06:12 +0100

    Add protoype for manging player jumps, speed, and collision

--- src/modules/players.js
+++ src/modules/players.js
@@ -18,4 +18,30 @@ class Player {
 
 Player.prototype.size = new Vec(0.8, 1.5);
 
+const playerXSpeed = 7;
+const gravity = 30;
+const jumpSpeed = 17;
+
+Player.prototype.update = function update(time, state, keys) {
+  let xSpeed = 0;
+  if (keys.ArrowLeft) xSpeed -= playerXSpeed;
+  if (keys.ArrowRight) xSpeed += playerXSpeed;
+
+  let { pos } = this;
+  const movedX = pos.plus(new Vec(xSpeed * time, 0));
+  if (!state.level.touches(movedX, this.size, 'wall')) pos = movedX;
+
+  let ySpeed = this.speed.y + time * gravity;
+  const movedY = pos.plus(new Vec(0, ySpeed * time));
+  if (!state.level.touches(movedY, this.size, 'wall')) {
+    pos = movedY;
+  } else if (keys.ArrowUp && ySpeed > 0) {
+    ySpeed = -jumpSpeed;
+  } else {
+    ySpeed = 0;
+  }
+
+  return new Player(pos, new Vec(xSpeed, ySpeed));
+};
+
 export default Player;
