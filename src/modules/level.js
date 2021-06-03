14597e5cf85d32cbd602a93f6ba992b5954b8227
George <71434803+george-swift@users.noreply.github.com>
2021-06-03 12:09:06 +0100

    Add prototype for managing positions

--- src/modules/level.js
+++ src/modules/level.js
@@ -14,7 +14,7 @@ const levelChars = {
   v: Lava,
 };
 
-export default class Level {
+class Level {
   constructor(plan) {
     const rows = plan.trim().split('\n').map((l) => [...l]);
     this.height = rows.length;
@@ -30,3 +30,22 @@ export default class Level {
     }));
   }
 }
+
+Level.prototype.touches = function gridChecker(position, size, type) {
+  const xStart = Math.floor(position.x);
+  const xEnd = Math.ceil(position.x + size.x);
+  const yStart = Math.floor(position.y);
+  const yEnd = Math.ceil(position.y + size.y);
+
+  for (let y = yStart; y < yEnd; y += 1) {
+    for (let x = xStart; x < xEnd; x += 1) {
+      const isOutside = x < 0 || x >= this.width || y < 0 || y >= this.height;
+      const here = isOutside ? 'wall' : this.rows[y][x];
+      if (here === type) return true;
+    }
+  }
+
+  return false;
+};
+
+export default Level;
