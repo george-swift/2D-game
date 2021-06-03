e4b66eb22a2e6d13baa6301dbd822d8fc645ef62
George <71434803+george-swift@users.noreply.github.com>
2021-06-03 12:05:00 +0100

    Add prototype for updating game state when player touches lava

--- src/modules/lava.js
+++ src/modules/lava.js
@@ -1,4 +1,5 @@
 /* eslint-disable consistent-return */
+import State from './state.js';
 import Vec from './vector.js';
 
 class Lava {
@@ -27,5 +28,18 @@ class Lava {
 }
 
 Lava.prototype.size = new Vec(1, 1);
+
+Lava.prototype.collide = (state) => new State(state.level, state.actors, 'lost');
+
+Lava.prototype.update = function update(time, state) {
+  const newPos = this.pos.plus(this.speed.times(time));
+
+  if (!state.level.touches(newPos, this.size, 'wall')) {
+    return new Lava(newPos, this.speed, this.reset);
+  } if (this.reset) {
+    return new Lava(this.reset, this.speed, this.reset);
+  }
+  return new Lava(this.pos, this.speed.times(-1));
+};
 
 export default Lava;
